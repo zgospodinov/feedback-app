909467c176fd8e2a394040346e1b0b6c19f056f0
Zdravko Stefanov Gospodinov <zgosp@softserveinc.com>
2022-08-18 21:31:20 +0300

    Remove commented code

--- src/index.js
+++ src/index.js
@@ -5,11 +5,4 @@ import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('root')
 const root = createRoot(container)
-root.render(<App/>)
-
-
-// ReactDOM.render(
-//     <React.StrictMode>
-//         <App />
-//     </React.StrictMode>
-//     , document.getElementById('root'))
+root.render(<App />)
